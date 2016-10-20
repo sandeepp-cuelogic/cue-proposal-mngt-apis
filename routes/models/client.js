b53525a868fba2d71ede41047417ee6ab3a41083
tusharncuelogic <tushar.nikam@cuelogic.co.in>
2016-10-20 18:37:40 +0530

    First commit

--- routes/models/client.js
+++
@@ -1,7 +0,0 @@
-var md5= require('md5') ;
-var DB = require('../config.js') ;
-var Promise = require('bluebird');
-
-module.exports.clients = function(req,res) {
-      res("This is client listing") ;
-}
